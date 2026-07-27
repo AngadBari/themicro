422e7ca99c2dc7999dc2445cebefd36fde5b6519
AngadBari <angadbari2@gmail.com>
2026-07-27 20:46:47 +0530

    add Carousel

--- app/components /button/page.tsx
+++
@@ -1,6 +0,0 @@
-import Category from "@/app/components /section/Category";
-import { buttons } from "@/app/components /button/data";
-
-export default function Page() {
-  return <Category data={buttons} />;
-}
