c5675c753540b878c9971854f826e38de1ae0f7c
AngadBari <angadbari2@gmail.com>
2026-07-27 20:50:50 +0530

    error

--- app/components /ui/page.tsx
+++
@@ -1,6 +0,0 @@
-import Category from "@/app/components /section/Category";
-import { cards } from "@/app/components /card/data"
-
-export default function Page() {
-  return <Category data={cards} />;
-}
